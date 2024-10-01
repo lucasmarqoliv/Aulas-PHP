3279faffd99f6e308f0fe1bd552f3a3ac3931db1
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-10-01 11:19:36 -0300

    upload

---
+++ questao02.php
@@ -0,0 +1,8 @@
+<?php
+    $a = 10;
+    $b = 15;
+    $soma = $a + $b;
+    echo 'Resultado da soma: ',$soma,'.';
+    $subtracao = $a - $b;
+    echo 'Resultado da subtração: ',$subtracao,'.';
+?>
