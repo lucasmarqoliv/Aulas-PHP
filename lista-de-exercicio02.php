09ce7804cf62fc901eb75b308fda776ddacf549f
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-10-02 10:30:15 -0300

    up

---
+++ lista-de-exercicio02.php
@@ -0,0 +1,14 @@
+<?php
+
+    //1 - Contagem de 1 a 10: Crie um loop for que imprima os números de 1 a 10.
+    for ($i = 1; $i <= 10; $i ++) {
+        echo $i;
+    }
+
+    //2 - Tabuada do 5: Utilize um loop for para imprimir a tabuada do 5 (de 5x1 a 5x10).
+    $numero = 5;
+    for ($i = 1; $i <=10; $i++) {
+        echo $numero * $i;
+    }
+
+?>
