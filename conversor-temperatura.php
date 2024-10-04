675484aaf6a8ee4930f38eb82ebaf6d44b63105c
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-10-04 11:18:33 -0300

    up

--- conversor-temperatura.php
+++ conversor-temperatura.php
@@ -1,13 +1,13 @@
 <?php
     $temperatura = $_POST['temperatura'];
     $escala = $_POST['escala'];
-    if ($escala == 'F') {
-        $temperatura = ($temperatura * 1.8) + 32;
-        echo 'A Temperatura em Fahrenheit é: ' .$temperatura;
-    }
-    elseif ($escala == 'C') {
+    if ($escala == 'C') {
         $temperatura = ($temperatura - 32) * 5.9;
-        echo 'A temperatura em Celsius é: ' .$temperatura;
+        echo 'A Temperatura em Celsius é: ' .$temperatura;
+    }
+    elseif ($escala == 'F') {
+        $temperatura = ($temperatura * 1.8) + 32;
+        echo 'A temperatura em Fahrenheit é: ' .$temperatura;
     }
     else {
         echo 'Digite a temperatura correta!';
