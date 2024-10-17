c23d99bd2f248ecc409627350b17a5db7b25af2a
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-10-17 09:53:29 -0300

    up

--- lista de exercicios 04/lista.php
+++ lista de exercicios 04/lista.php
@@ -1,10 +1,23 @@
+<style>
+        #titulo {
+            text-align: center;
+        }
+        #tabela {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            margin-top: 100px;
+        }
+</style>
+
 <?php
 // Lê os dados do arquivo
 $arquivo = "dados.txt";
 $usuarios = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-echo "<h1>Lista de Usuários</h1>";
-echo "<table border='1'>";
+echo "<h1 id='titulo'>Lista de Usuários</h1>";
+echo "<table border='1' id='tabela'>";
 echo "<tr><th>Nome</th><th>Email</th><th>Data de Nascimento</th><th>CPF</th><th>RG</th></tr>";
 
 foreach ($usuarios as $usuario) {
